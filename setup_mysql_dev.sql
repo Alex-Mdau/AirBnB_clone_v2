8162c048c3fcea4ce54034f6a4cefdc0501c7724
Kelvin-Mbugua <111062830+Kelvin-Mbugua@users.noreply.github.com>
2023-03-22 22:46:35 +0300

    Add files via upload

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,7 @@
--- this script prepares a MySQL server for the project
--- create project developement database with the name : hbnb_dev_db
+-- Creates a database called hbnb_dev_db in the current MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
--- with the password : hbnb_dev_pwd if it dosen't exist
+-- creates the MySQL server user hbnb_test
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- granting all privileges to the new user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
--- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+-- Grants Permissions for user hbnb_test
+GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
